18ed0331f2ff7e6c704d0a6cf7a8a87bbd340608
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2024-12-18 21:09:35 +0100

    Add routing configuration with routes for Home, Vehicles, Real Estate, Login, and Register components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { RealEstateComponent } from './real-estate/real-estate.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    { 
+        path: 'vehicles', 
+        component: VehiclesComponent 
+    },
+    { 
+        path: 'real-estate', 
+        component: RealEstateComponent
+    },
+    { 
+        path: 'login', 
+        component: LoginComponent 
+    },
+    { 
+        path: 'register',
+         component: RegisterComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }

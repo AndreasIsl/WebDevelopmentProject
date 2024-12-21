3f94979a71a49cfcd592329e99696f7acee7c041
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2024-12-21 10:50:08 +0100

    Enhance LoginComponent with form structure and validation logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+;
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log('Form Submitted!', form.value);
+    }
+  }
 }

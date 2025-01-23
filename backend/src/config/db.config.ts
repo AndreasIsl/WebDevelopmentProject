edcd7ee5ece9515e33966d1a1612fd826be3465a
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2025-01-23 16:45:49 +0100

    env substituted cause they dont want to work

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -2,20 +2,29 @@ import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 
-dotenv.config();
+dotenv.config({path : '.env'});
 
-console.log(process.env['DB_USER']);
-console.log(process.env['DB_HOST']);
-console.log(process.env['DB_NAME']);
-console.log(process.env['DB_PASSWORD']);
+// console.log(process.env.REACT_APP_DB_USER);
+// console.log(process.env.REACT_APP_DB_HOST);
+// console.log(process.env.REACT_APP_DB_NAME);
+// console.log(process.env.REACT_APP_DB_PASSWORD);
+
+// const pool = new Pool({
+//     user: process.env['DB_USER'],
+//     host: process.env['DB_HOST'],
+//     database: process.env['DB_NAME'],
+//     password: process.env['DB_PASSWORD'] as string,
+//     port: process.env['DB_PORT'] ? parseInt(process.env['DB_PORT']) : undefined,
+// });
 
 const pool = new Pool({
-    user: process.env['DB_USER'],
-    host: process.env['DB_HOST'],
-    database: process.env['DB_NAME'],
-    password: process.env['DB_PASSWORD'] as string,
-    port: process.env['DB_PORT'] ? parseInt(process.env['DB_PORT']) : undefined,
+    user: "postgres",
+    host: "localhost",
+    database: "WebdevProject",
+    password: "admin",
+    port: 5432,
 });
+
 
 // Test the database connection
 pool.query('SELECT NOW()', (err, res) => {

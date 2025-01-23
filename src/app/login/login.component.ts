cb9720b33bd8a3d72d8a658b28d99f568823cfd4
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2025-01-23 17:00:54 +0100

    added login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { RouterLink, Router } from '@angular/router';
-import { NgIf } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl, ValidationErrors } from '@angular/forms';
+import { RouterLink, Router, RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl, ValidationErrors, FormsModule } from '@angular/forms';
 import { ApiService } from '../api.service';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent, User } from '../app.component';
 
 
@@ -13,18 +10,18 @@ import { AppComponent, User } from '../app.component';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrls: ['./login.component.css'],
   providers: [ApiService]
 })
 export class LoginComponent {
-  registerForm: FormGroup;
+  loginForm: FormGroup;
   @ViewChild('notUniqueUserAlert') notUniqueUserAlert!: ElementRef<HTMLParagraphElement>;
 
 
   constructor(private fb: FormBuilder, private apiService: ApiService, private router: Router, private appComponent: AppComponent) {
-    this.registerForm = this.fb.group({
+    this.loginForm = this.fb.group({
       userName: ['', [Validators.required]],
       password: ['', [Validators.required]],
     });
@@ -32,16 +29,13 @@ export class LoginComponent {
 
   async onSubmit() {
     const userData = {
-      UserName: this.registerForm.value.userName,
-      Password: this.registerForm.value.password,
+      UserName: this.loginForm.value.userName,
+      Password: this.loginForm.value.password,
     };
-
     const isUserValid = await this.loginCheck(userData.UserName, userData.Password);
 
-    if (this.registerForm.valid && isUserValid) {
-      // const user = new User(userData.UserName, userData.Password, userData.Email, '');
-      // this.appComponent.setCurrentUser(user);
-      console.log('Form Submitted!');
+    if (this.loginForm.valid && isUserValid) {
+      this.appComponent.onLogin(userData.UserName, userData.Password);
       this.router.navigate(['']);
     } else {
 
@@ -49,26 +43,19 @@ export class LoginComponent {
 
   }
 
-  loginCheck(username: string, password:string): Promise<boolean> {
+  loginCheck(username: string, password: string): Promise<boolean> {
     return new Promise((resolve, reject) => {
-      fetch('http://localhost:5000/authen/user/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ UserName: username , Password: password}),
+      fetch(`http://localhost:5000/authen/user/login?UserName=${username}&Password=${password}`, {
+        method: 'GET',
       }).then(async (response) => {
-        console.log(`Error: ${response.body}`);
-        const data = await response.json();
-
-        // if (!response.ok) {
-        //   console.error('Error: User already exists');
-        //   this.notUniqueUserAlert.nativeElement.style.display = 'block';
-        //   resolve(false);
-        // } else {
-        //   console.log(`Error: ${response.body}`);
-        //   resolve(true);
-        // }
+        if (!response.ok) {
+          console.error('Error: User already exists');
+          this.notUniqueUserAlert.nativeElement.style.display = 'block';
+          resolve(false);
+        } else {
+          console.log(`Login data matches user`);
+          resolve(true);
+        }
       }).catch((err) => {
         console.error('Network error:', err);
         alert('Failed to communicate with the server');
@@ -78,7 +65,9 @@ export class LoginComponent {
   }
 
   closeAlert() {
+
     if (this.notUniqueUserAlert) {
+
       this.notUniqueUserAlert.nativeElement.style.display = 'none';
     } else {
       console.error('Element not found: notUniqueUserAlert');

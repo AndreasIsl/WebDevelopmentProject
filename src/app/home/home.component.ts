5b2b97171ff4d9b072f66318b7166a2d83a7768b
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2025-02-16 15:05:36 +0100

    add item slideshow to home component wip

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
-  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+slideRight() {
+throw new Error('Method not implemented.');
+}
+slideLeft() {
+throw new Error('Method not implemented.');
+}
+  vehicles: any[] = [];
+  real_estate: any[] = [];
 
+  constructor() {
+    this.getVehicles();
+    // this.getRealEstate();
+  }
+
+  async getVehicles() {
+    try {
+      const response = await fetch('http://localhost:5000/vehicles');
+      const data = await response.json();
+      this.vehicles = data;
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      console.log(data);
+
+      this.setPlaceholderImage();
+     } catch (err) {
+      console.error(err);
+    }
+  }
+  
+  setPlaceholderImage() {
+    this.vehicles.forEach((vehicle) => {
+      if (vehicle.bilder.length == 0) {
+        vehicle.Image = '../assets/images/car_placeholder_image.png';
+      } else {
+        vehicle.Image = vehicle.bilder[0];
+      }
+    })
+  }
+
+  async getRealEstate() {
+  }
 }

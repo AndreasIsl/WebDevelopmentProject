346ab01b99213ccb32f71118d70aad55f6a21519
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2025-03-03 12:35:00 +0100

    add constructor and imports

--- src/app/new-listing/vehicle-listing/vehicle-listing.component.ts
+++ src/app/new-listing/vehicle-listing/vehicle-listing.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AppComponent } from '../../app.component';
+import { passwordValidator, confirmPasswordValidator } from '../../register/register.component';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-vehicle-listing',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './vehicle-listing.component.html',
   styleUrl: './vehicle-listing.component.css'
 })
 export class VehicleListingComponent {
+  vehicleListingForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router, private appComponent: AppComponent, private authService: AuthService) {
+    this.vehicleListingForm = this.fb.group({
+      brand: ['', [Validators.required]],
+      model: ['', [Validators.required]],
+      manufactoringDate: ['', [Validators.required]],
+      mileage: ['', Validators.required],
+      price: ['', Validators.required]
+    }, { validators: confirmPasswordValidator });
+  }
+  
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
 }
